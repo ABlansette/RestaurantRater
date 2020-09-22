1efb02c38a86eb092d1fc0d906c0654ffa83e9c8
Alexander.Blansette@gmail.com <Alexander.Blansette@gmail.com>
2020-09-22 15:46:42 -0400

    Delete

--- SuperRealRestaurantRater/Controllers/RestaurantController.cs
+++ SuperRealRestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,14 +28,36 @@ namespace SuperRealRestaurantRater.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Restaurant restaurant)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Restaurants.Add(restaurant);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(restaurant);
-            
+
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
